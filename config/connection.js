b84d472c2efc4ce7dd430e6d80ed3a34e0fdb653
Ashley Hunt <ashhunt07@gmail.com>
2020-10-21 23:47:29 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,26 +2,24 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    // Your port from MYSQL
-    port: 3306,
-    // Your username
-    user: "root",
-    // Your password
-    password: process.env.MYSQLPW,
-    // Primary Database
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        // Your port from MYSQL
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: process.env.MYSQLPW,
+        // Primary Database
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
